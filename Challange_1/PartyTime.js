5b5c2d46b4a09ea485b2f16b0a2a2118a863a57a
Slaykha <kadirengin2000@hotmail.com>
2022-09-17 20:02:19 +0300

    add audio analyzer

--- Challange_1/PartyTime.js
+++ Challange_1/PartyTime.js
@@ -12,6 +12,43 @@ function getTime(){
     }, interval);
 }
 
+function audioAnalyzer(){
+    const ctx = new AudioContext();
+    const audioSrc = ctx.createMediaElementSource(opening);
+    const analyser = ctx.createAnalyser();
+
+    audioSrc.connect(analyser);
+    analyser.connect(ctx.destination);
+
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.frequencyBinCount
+    const frequencyData = new Uint8Array(bufferLength)
+
+    setInterval(() => {
+        analyser.getByteTimeDomainData(frequencyData);
+        let dataArray = Object.values(frequencyData);
+        if (dataArray[50] > 100) {  
+          /*window.requestAnimationFrame(
+            await changeBgColor(dataArray)
+          )*/
+          changeBgColor()
+        }
+      }, 500);
+}
+
+const changeBgColor = () => {
+    var colorArray = ["red", "yellow", "green", "blue"]
+    var background = document.body
+
+    background.style.background = colorArray[number]
+
+    if(number < 3){
+        number = number + 1;
+    }else{
+        number = 0
+    }
+}
+
 function buttonClick(){
     document.body.style.background = "black";
     var button = document.getElementById("button")
@@ -35,7 +72,14 @@ function buttonClick(){
             }, 600);
         }, 2000);
     }, 1500);
+}
 
+const expandSquare = () => {
+    square.classList.add("squareExpand")
+}
+
+const Square = () => {
+    square.classList.add("square")
 }
 
 function lifeGoesOn(){
@@ -43,21 +87,21 @@ function lifeGoesOn(){
     var lifeGoesOn = true
 
         setTimeout(() => {
-            background.style.background = "red"
+            changeBgColor()
             setTimeout(() => {
-                background.style.background = "blue"
+                changeBgColor()
                 setTimeout(() => {
-                    background.style.background = "green"
+                    changeBgColor()
                     setTimeout(() => {
-                        background.style.background = "yellow"
+                        changeBgColor()
                         setTimeout(() => {
-                            background.style.background = "red"
+                            changeBgColor()
                             setTimeout(() => {
-                                background.style.background = "blue"
+                                changeBgColor()
                                 setTimeout(() => {
-                                    background.style.background = "green"
+                                    changeBgColor()
                                     setTimeout(() => {
-                                        background.style.background = "yellow"
+                                        changeBgColor()
                                         setTimeout(() => {
                                             opening.pause()
                                         }, 500);
@@ -72,8 +116,12 @@ function lifeGoesOn(){
 
 
 }
+var number = 0
+const square = document.getElementById("square")
+
 
 var opening = new Audio("sounds/openinng2.wav")
+
 document.getElementById("button").classList.add("buttonFeatures")
 
 
